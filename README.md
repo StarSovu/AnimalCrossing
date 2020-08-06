ff68dd87330ad18e9cf9c75c099e9c924d736a5d
sovu <sonja.vuorinen@helsinki.fi>
2020-08-07 00:04:30 +0300

    admin properties added, readme update, some sort of password check

--- README.md
+++ README.md
@@ -1,41 +1,69 @@
 # AnimalCrossing
 tsoha 2020
 
+Tehtävä ennen sunnuntain palautusta:
+Admin-sivu jolla:
+Annetaan hahmolle nimi, valitaan hahmolle laji, persoonallisuus ja asu (valinnat radio-button) sekä annetaan syntymäpäivä. Tarkastetaan että kaikki tiedot on annettu, ei saa jäädä mikään kenttä tyhjäksi. Laitetaan myös visible = 1.
+Admin-sivu jolla (voi olla sama kuin edellinen sivu?):
+Muokataan olemassa olevia hahmoja (nimen muutos, syntymäpäivän muutos, lajin, persoonallisuuden ja asun vaihtaminen) 
+User-sivu jolla:
+valitaan saarelle hahmot ja asut
+Päivitetään readme.md; Animal Crossing kuvaus ja ohjelman toimintaa tarkemmin.
 
-Application for users to create their own island and select Animal Crossing characters for their island.
 
-Each user has a username (unique) and password. Users can also name their island, which is different from the username and not necessarily unique. Each user can have multiple islands (every island from the same user must have a different name), and each island can have a maximum of 10 animals.
 
-Only a user with admin rights can create and edit characters. All users can select and change characters for their own island. If the island is full and the user tries to add another character, they will get a message saying that they need to kick someone off their island before they can add another character. Users cannot have the same character multiple times, but multiple users can have the same character on their island.
 
-Characters have: name (unique), birthday (month and day), species and personality.
-Personalities: Normal, Peppy, Snooty, Sisterly, Lazy, Cranky, Jock, Smug
+Animal Crossing is a social simulation video game series by Nintendo. In Animal Crossing, the player character is a human who lives in a village inhabited by various anthropomorphic animals, carrying out various activities such as fishing, bug catching, and fossil hunting. The series is notable for its open-ended gameplay and extensive use of the video game console's internal clock and calendar to simulate real passage of time. (Description based on Wikipedia article.) This application is particularly based on the newest game in the series, Animal Crossing: New Horizons, in which the player lives on an island with up to ten animal villagers.
 
-Any visitor can see the list of charactes and filter it by species and/or personality. Lists can be arranged alphabetically or by birthday. There is also a search function, which allows visitors to search for characters with specific names. Visitors can see for which islands each character is selected to.
+This application is for users to create their own island and select their ideal roster of Animal Crossing characters for their island.
+
+Each user has a username (unique) and password. Users can name islands for themselves, not necessarily unique names (several users can have an island with the same name). Each user can have multiple islands, every island from the same user must have a different name, and each island can have a maximum of 10 animals. Same animal can be selected on the same island only once.
+(Not yet done! Limit the number of islands / user.)
+
+Only a user with admin rights can create and edit characters. All users can select and change characters for their own island. If the island is full and the user tries to add another character, they will get a message saying that they need to kick someone off their island before they can add another character. Users cannot have the same character multiple times, but multiple users can have the same character on their island. Users can view other user's islands and the selected animals on them.
+
+(Not yet done!)
+Admin can hide users, islands, characters and character parameters.
+
+Characters have: name (unique), birthday (month and day), species, personality and outfit. Only user with admin rights can select birthday, species, personality and outfit for characters. (This not yet done!) Users can change the outfit for a character on a specific island. If the same animal is on multiple islands, it can have different outfit on different islands.
+
+
+(This not yet done!) Any visitor can see the list of charactes and filter it by species and/or personality. Lists can be arranged alphabetically or by birthday. There is also a search function, which allows visitors to search for characters with specific names. Visitors can see for which islands each character is selected to.
 
 Optional, after other features of the application have been compeleted:
-There are also outfits, which users can give to animals on their island. Each animal has a specific default outfit which they wear if the user does not change them. Each animal can only wear one outfit, but the same outfit can be given to multiple animals. If the same animal is on multiple islands, they can have different outfit on different islands, so users cannot change animals' outfits on other islands.
+There are also outfits, which users can give to animals on their island. Each animal has a specific default outfit which they wear if the user does not change them. Each animal can only wear one outfit, but the same outfit can be given to multiple animals. , so users cannot change animals' outfits on other islands.
 
 
 Application utlizes a database. In database there are tables:
 
-Users:
-Username - Password hash value - Rights(user/admin) - Visible (0/1)
+users:
+username - Password hash value(not yet done, just password as text!) - role(user/admin) - visible (0/1)
 
-Outfits:
-Id - Outfit name - Visible (0/1)
+outfits:
+id - outfitname - visible (0/1)
 
-Personalities:
-Id - Personality - Visible (0/1)
+personalities:
+id - personalityname - visible (0/1)
 
-Species:
-Id - Species - Visible (0/1)
+species:
+id - speciesname - visible (0/1)
 
-Characters:
-Id - character name - Species id - Birthday (MM/DD) - Personality id - Outfit id (default outfit) - Visible (0/1)
+characters:
+id - charactername - speciesid - personalityid - outfitid- birthday (MM/DD) - visible (0/1)
 
-Islands:
-Id island name - User id - Visible (0/1)
+islands:
+id - islandname - userid - visible (0/1)
 
-Character on island:
-Island id - Character id - Outfit id (outfit which the character wears on that specific island)
+characteronisland:
+island id- characterid - outfitid - visible (0/1)
+
+
+On the index.html site it is checked if user is logged in or not. If not, login and register possibilities are shown. Before login users need to register. Role 'admin' role is given directly in SQL, not by this application. Users automatically are given 'user' role. When registering it is checked that the username does not yet exists.
+
+After login user is directed to user's welcome page. From there user follows links to be able to view, create and edit their own islands. 
+
+At the moment user passwords are saved as text in the database, this will need to be changed to hash value. At the moment not all input formats are checked, it is for example possible to give blank values as input. This will need to be changed.
+
+Also looks and usability need improvements.
+
+
--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
  
 
  #hash_value = generate_password_hash(password)
@@ -28,34 +29,46 @@ def index():
 def login():
     username = request.form["username"]
     password = request.form["password"]
-    sql = "SELECT password FROM users WHERE username=:username"
+    sql = "SELECT username FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
-    user = result.fetchone()    
-    if user == None:
+    usercheck = result.fetchone()
+    if usercheck == None:
+     session["infotext"] = "Username does not exist."
      return redirect("/")
-     # TODO: invalid username, nyt vaan menään alkuun
-     # else:
+     # TODO: hash
+     #else:
      #  hash_value = user[0]
      #  if check_password_hash(hash_value,password):
-        # TODO: correct username and password
     else:
-        # TODO: invalid password
-        # TODO: user check
-     session["username"] = username
-     sql = "SELECT id FROM users WHERE username=:username"
+     sql = "SELECT password FROM users WHERE username=:username"
      result = db.session.execute(sql, {"username":username})
-     userid = result.fetchone()[0]
-     session["userid"] = userid
-     return redirect("/")
+     pwd = result.fetchone()[0]  
+     if password == pwd:
+      session["username"] = username
+      sql = "SELECT id FROM users WHERE username=:username"
+      result = db.session.execute(sql, {"username":username})
+      userid = result.fetchone()[0]
+      session["userid"] = userid
+      session["infotext"] = "q"
+      del session["infotext"]
+      return redirect("/")
+     else:
+      session["infotext"] = "Password not correct."
+      return redirect("/")
+
+     
    
 @app.route("/logout")
 def logout():
     del session["username"]
     del session["userid"]
+    session["infotext"] = "q"
+    del session["infotext"]
     return redirect("/")
 
 @app.route("/register",methods=["POST"])
 #talleettaa vielä salasanat suoraan kantaan, pitää muuttaa HASH-tallennus
+#ei mitään ehtoja username (paitsi pitää olla uniikki) ja password, esim. tyhjä käy. Pitää lisätä.
 def register():
     username = request.form["username"]
     password = request.form["password"]
@@ -63,21 +76,23 @@ def register():
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if user == None:
-     sql = "INSERT INTO users (username, password, role) VALUES (:username, :password, 'user')"
+     sql = "INSERT INTO users (username, password, role, visible) VALUES (:username, :password, 'user', 1)"
      db.session.execute(sql, {"username":username, "password":password})
-     db.session.commit()   
+     db.session.commit()
+     session["infotext"] = "q"
+     del session["infotext"]   
      return redirect("/")
     else:
-     return "Username taken"
-     #TODO: ohjaus jonnekin
+     session["infotext"] = "Username taken"
+     return redirect("/")
 
-@app.route("/user/<string:user>")
-def page(user):
+@app.route("/user/<int:id>")
+def page(id):
     #Kirjoittamalla html-osoitteeksi käyttäjän jota ei ole ohjelma kaatuu (mutta ei kai väliä?)
-    username = user
-    sql = "SELECT id FROM users WHERE username=:username"
-    result = db.session.execute(sql, {"username":username})
-    userid = result.fetchone()[0]
+    userid = id
+    sql = "SELECT username FROM users WHERE id=:userid"
+    result = db.session.execute(sql, {"userid":userid})
+    user = result.fetchone()[0]
     sql = "SELECT islandname FROM islands WHERE userid=:userid"
     result = db.session.execute(sql, {"userid":userid})
     islands = result.fetchall()
@@ -86,7 +101,11 @@ def page(user):
 
 @app.route("/createisland")
 def createisland():
-   return render_template("createisland.html")
+    userid = session["userid"]
+    sql = "SELECT islandname FROM islands WHERE userid=:userid"
+    result = db.session.execute(sql, {"userid":userid})
+    islands = result.fetchall()
+    return render_template("createisland.html", islands=islands)
     # TODO
 
 @app.route("/addisland",methods=["POST"])
@@ -100,10 +119,168 @@ def addisland():
      sql = "INSERT INTO islands (islandname, userid, visible) VALUES (:islandname, :userid, 1)"
      db.session.execute(sql, {"islandname":islandname, "userid":userid})
      db.session.commit()
-     return redirect("/")
+     return redirect("/createisland")
     else:
      return "You already have an island with this name."
      #TODO: ohjaus jonnekin
+
+@app.route("/editcharacters")
+def editcharacters():
+    userid = session["userid"]
+    sql = "SELECT role FROM users WHERE id=:userid AND visible=1"
+    result = db.session.execute(sql, {"userid":userid})
+    role = result.fetchone()[0]
+    if role == "admin":
+     sql = "SELECT personalityname FROM personalities WHERE visible=1"
+     result = db.session.execute(sql)
+     personalities = result.fetchall()
+     sql = "SELECT speciesname FROM species WHERE visible=1"
+     result = db.session.execute(sql)
+     species = result.fetchall()
+     sql = "SELECT outfitname FROM outfits WHERE visible=1"
+     result = db.session.execute(sql)
+     outfits = result.fetchall()
+     return render_template("editcharacters.html", personalities=personalities, species=species, outfits=outfits)
+    else:
+     return "You don't have permission."
+
+@app.route("/personalities")
+def personalities():
+    userid = session["userid"]
+    sql = "SELECT role FROM users WHERE id=:userid AND visible=1"
+    result = db.session.execute(sql, {"userid":userid})
+    role = result.fetchone()[0]
+    if role == "admin":
+     sql = "SELECT personalityname FROM personalities WHERE visible=1"
+     result = db.session.execute(sql)
+     personalities = result.fetchall()
+     return render_template("personalities.html", personalities=personalities)
+    else:
+     return "You don't have permission."
+
+@app.route("/addpersonality",methods=["POST"])
+def addpersonality():
+    personality = request.form["personality"]
+    sql = "SELECT personalityname FROM personalities WHERE personalityname=:personality"
+    result = db.session.execute(sql, {"personality":personality})
+    checkpersonality = result.fetchone()
+    if checkpersonality == None:
+     sql = "INSERT INTO personalities (personalityname, visible) VALUES (:personality, 1)"
+     db.session.execute(sql, {"personality":personality})
+     db.session.commit()   
+     return redirect("/personalities")
+    else:
+     return "A personality with this name already exists."
+
+@app.route("/editpersonality", methods=["POST"])
+def editpersonality():
+    personality = request.form["old"]
+    new = request.form["new"]
+    sql = "SELECT personalityname FROM personalities WHERE personalityname=:new AND visible=1"
+    result = db.session.execute(sql, {"new":new})
+    checkpersonality = result.fetchone()
+    if checkpersonality == None:
+     sql = "SELECT id FROM personalities WHERE personalityname=:personality"
+     result = db.session.execute(sql, {"personality":personality})
+     id = result.fetchone()[0]
+     sql = "UPDATE personalities SET personalityname=:new WHERE id=:id"
+     db.session.execute(sql, {"new":new, "id":id})
+     db.session.commit()
+     return redirect("/personalities")
+    else:
+     return "A personality with this name already exists."
+
+@app.route("/species")
+def species():
+    userid = session["userid"]
+    sql = "SELECT role FROM users WHERE id=:userid AND visible=1"
+    result = db.session.execute(sql, {"userid":userid})
+    role = result.fetchone()[0]
+    if role == "admin":
+     sql = "SELECT speciesname FROM species WHERE visible=1"
+     result = db.session.execute(sql)
+     species = result.fetchall()
+     return render_template("species.html", species=species)
+    else:
+     return "You don't have permission."
+
+@app.route("/addspecies",methods=["POST"])
+def addspecies():
+    species = request.form["species"]
+    sql = "SELECT speciesname FROM species WHERE speciesname=:species"
+    result = db.session.execute(sql, {"species":species})
+    checkspecies = result.fetchone()
+    if checkspecies == None:
+     sql = "INSERT INTO species (speciesname, visible) VALUES (:species, 1)"
+     db.session.execute(sql, {"species":species})
+     db.session.commit()   
+     return redirect("/species")
+    else:
+     return "A species with this name already exists."
+
+@app.route("/editspecies", methods=["POST"])
+def editspecies():
+    species = request.form["old"]
+    new = request.form["new"]
+    sql = "SELECT speciesname FROM species WHERE speciesname=:new AND visible=1"
+    result = db.session.execute(sql, {"new":new})
+    checkspecies = result.fetchone()
+    if checkspecies == None:
+     sql = "SELECT id FROM species WHERE speciesname=:species"
+     result = db.session.execute(sql, {"species":species})
+     id = result.fetchone()[0]
+     sql = "UPDATE species SET speciesname=:new WHERE id=:id"
+     db.session.execute(sql, {"new":new, "id":id})
+     db.session.commit()
+     return redirect("/species")
+    else:
+     return "A species with this name already exists."
+
+@app.route("/outfits")
+def outfits():
+    userid = session["userid"]
+    sql = "SELECT role FROM users WHERE id=:userid AND visible=1"
+    result = db.session.execute(sql, {"userid":userid})
+    role = result.fetchone()[0]
+    if role == "admin":
+     sql = "SELECT outfitname FROM outfits WHERE visible=1"
+     result = db.session.execute(sql)
+     outfits = result.fetchall()
+     return render_template("outfits.html", outfits=outfits)
+    else:
+     return "You don't have permission."
+
+@app.route("/addoutfit",methods=["POST"])
+def addoutfit():
+    outfit = request.form["outfit"]
+    sql = "SELECT outfitname FROM outfits WHERE outfitname=:outfit"
+    result = db.session.execute(sql, {"outfit":outfit})
+    checkoutfit = result.fetchone()
+    if checkoutfit == None:
+     sql = "INSERT INTO outfits (outfitname, visible) VALUES (:outfit, 1)"
+     db.session.execute(sql, {"outfit":outfit})
+     db.session.commit()   
+     return redirect("/outfits")
+    else:
+     return "An outfit with this name already exists."
+
+@app.route("/editoutfit", methods=["POST"])
+def editoutfit():
+    outfit = request.form["old"]
+    new = request.form["new"]
+    sql = "SELECT outfitname FROM outfits WHERE outfitname=:new AND visible=1"
+    result = db.session.execute(sql, {"new":new})
+    checkoutfit = result.fetchone()
+    if checkoutfit == None:
+     sql = "SELECT id FROM outfits WHERE outfitname=:outfit"
+     result = db.session.execute(sql, {"outfit":outfit})
+     id = result.fetchone()[0]
+     sql = "UPDATE outfits SET outfitname=:new WHERE id=:id"
+     db.session.execute(sql, {"new":new, "id":id})
+     db.session.commit()
+     return redirect("/outfits")
+    else:
+     return "An outfit with this name already exists."+new +outfit
 
 
 @app.route("/island")
